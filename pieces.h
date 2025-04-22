c2a630af376f868634be01a03a315d1443748796
Christian Goss <gosscj1@appstate.edu>
2025-04-22 17:35:24 -0400

    finally??

--- pieces.h
+++ pieces.h
@@ -4,11 +4,7 @@
 #include "types.h"
 #include "position.h"
 
-// Returns the base value of a piece type in centipawns
 int piece_value(PieceType pt);
-
-// Evaluates the current position from the perspective of the given color
 int evaluate(const Position& pos, Color perspective);
-
 int alpha_beta(Position& pos, int depth, int alpha, int beta, Color side);
 Move find_best_move_alpha_beta(Position& pos, Color side, int depth);
