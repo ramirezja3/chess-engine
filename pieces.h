f29bdce170795679ab11112afab1f40af3ae110f
Christian Goss <gosscj1@appstate.edu>
2025-04-22 16:59:23 -0400

    finally??

--- pieces.h
+++ pieces.h
@@ -9,3 +9,4 @@ int piece_value(PieceType pt);
 
 // Evaluates the current position from the perspective of the given color
 int evaluate(const Position& pos, Color perspective);
+Move find_best_move_alpha_beta(Position& pos, Color side, int depth);
